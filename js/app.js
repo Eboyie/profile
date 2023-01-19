7b9e7346b6528ca8a4abf51ced03bf290b63c567
Francis Eboyie <aklinlibie2008@gmail.com>
2023-01-19 22:17:52 +0200

    pre-profile

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//const navbar = document.querySelector("#nav");
+
+const date = document.querySelector("#date");
+
+// add fixed class to navbar
+/*window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
+});
+*/
+// set year
+date.innerHTML = new Date().getFullYear();
